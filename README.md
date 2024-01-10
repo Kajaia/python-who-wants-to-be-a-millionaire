c4565a0daa74b0814b988d419b5934d84863972f
Kajaia <lashakajaia31@gmail.com>
2024-01-10 20:54:52 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Who Wants To Be a Millionaire?
+
+Popular television game show in your terminal.
+
+To start playing run this command `python script.py`
---
+++ script.py
@@ -0,0 +1,44 @@
+questions = [
+    {'question': 'Who is known for his contributions to the field of computer science and is often referred to as the "father of modern computer science"?', 'answers': {'a': 'Bill Gates', 'b': 'Alan Turing', 'c': 'Steve Jobs', 'd': 'Mark Zuckerberg'}, 'correct': 'b'},
+    {'question': 'Which programming language is often used for web development?', 'answers': {'a': 'Java', 'b': 'Python', 'c': 'HTML', 'd': 'Swift'}, 'correct': 'c'},
+    {'question': 'What does PHP stand for?', 'answers': {'a': 'Personal Home Page', 'b': 'Hypertext Markup Language', 'c': 'Python Hypertext Processor', 'd': 'Programming Hyper Processor'}, 'correct': 'a'},
+    {'question': 'Which JavaScript framework is often used for building user interfaces?', 'answers': {'a': 'Angular', 'b': 'Django', 'c': 'Flask', 'd': 'Express'}, 'correct': 'a'},
+    {'question': 'What does the "DOM" stand for in web development?', 'answers': {'a': 'Document Object Model', 'b': 'Data Object Model', 'c': 'Dynamic Object Mechanism', 'd': 'Document Oriented Middleware'}, 'correct': 'a'},
+    {'question': 'Which Python framework is commonly used for web development?', 'answers': {'a': 'Django', 'b': 'Flask', 'c': 'Ruby on Rails', 'd': 'Express.js'}, 'correct': 'a'},
+    {'question': 'What is the purpose of the "elif" keyword in Python?', 'answers': {'a': 'Loop iteration', 'b': 'Exception handling', 'c': 'Conditional branching', 'd': 'Function definition'}, 'correct': 'c'},
+    {'question': 'Which of the following is a Python data type for storing a sequence of elements?', 'answers': {'a': 'Dictionary', 'b': 'List', 'c': 'Tuple', 'd': 'Set'}, 'correct': 'b'},
+    {'question': 'What is the purpose of "git" in software development?', 'answers': {'a': 'Database management', 'b': 'Version control', 'c': 'Package management', 'd': 'Testing automation'}, 'correct': 'b'},
+    {'question': 'Which method is used to get the length of a list in Python?', 'answers': {'a': 'size()', 'b': 'length()', 'c': 'count()', 'd': 'len()'}, 'correct': 'd'},
+    {'question': 'In Python, what does the "+= 1" operation do?', 'answers': {'a': 'Subtract 1', 'b': 'Add 1', 'c': 'Multiply by 1', 'd': 'Assign value 1'}, 'correct': 'b'},
+    {'question': 'Which of the following is a benefit of using virtual environments in Python development?', 'answers': {'a': 'Faster code execution', 'b': 'Easier dependency management', 'c': 'Higher security', 'd': 'Improved syntax'}, 'correct': 'b'},
+    {'question': 'What does the acronym "API" stand for?', 'answers': {'a': 'Application Programming Interface', 'b': 'Automated Program Integration', 'c': 'Advanced Programming Interface', 'd': 'Application Program Interface'}, 'correct': 'a'},
+    {'question': 'Which Python library is commonly used for data visualization?', 'answers': {'a': 'Matplotlib', 'b': 'NumPy', 'c': 'Pandas', 'd': 'SciPy'}, 'correct': 'a'},
+    {'question': 'What is the primary purpose of a constructor in object-oriented programming?', 'answers': {'a': 'Data manipulation', 'b': 'Code organization', 'c': 'Object instantiation', 'd': 'Memory optimization'}, 'correct': 'c'}
+]
+
+print(questions[0])
+
+# Should give us a random question with answers and its number
+class Question:
+    pass
+
+# Should get user answer and tell us if it is correct
+class Answer:
+    pass
+
+# Should receive a question number ang return money won
+class Award:
+    pass
+
+# Player should have a name
+# We should save answered questions
+# We should have money won
+class Player:
+    pass
+
+# Help types: ['50:50', 'Audience', 'Call']
+# 50:50 - Give a user two answers, one correct and one incorrect
+# Audience - Give a user two answers, one correct with percentage from 60 to 80, one incorrect with percentage left
+# Call - Give a user some text from his/her friend or family member with correct answer
+class Help:
+    pass
